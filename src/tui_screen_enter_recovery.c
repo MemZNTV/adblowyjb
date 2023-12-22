7650a27ec70a382a7327e451553801842a8a0762
MemPlayZ <75344653+DriftPlayZ@users.noreply.github.com>
2023-12-21 21:58:25 -0500

    based on palera1n

---
+++ src/tui_screen_enter_recovery.c
@@ -0,0 +1,21 @@
+#ifdef TUI
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <locale.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <palerain.h>
+#include <tui.h>
+
+tui_screen_t tui_screen_enter_recovery(void) { return ENTER_DFU_SCREEN; }
+
+#else
+/* ISO C forbids an empty translation unit */
+extern char** environ;
+#endif
